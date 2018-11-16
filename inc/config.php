959a58ce8a49e0470361ebf0cae9e84990750c77
geiyawp <geiyawp04@gmail.com>
2018-11-16 10:25:57 +0700

    Delete config.php

--- inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "news";
-
-$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
