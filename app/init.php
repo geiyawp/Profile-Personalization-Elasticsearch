0076e52db8dc18cbc05a809725742de91e6164f5
geiyawp <geiyawp04@gmail.com>
2018-11-16 10:38:52 +0700

    Update init.php

--- app/init.php
+++ app/init.php
@@ -4,16 +4,10 @@ require_once 'vendor/autoload.php';
 
 use Elasticsearch\ClientBuilder;
 
-$hosts = ['127.0.0.1:9200'];              // Replace with your host
-$client = ClientBuilder::create()           // Instantiate a new ClientBuilder
-	->setHosts($hosts)      // Set the hosts
-    ->build();              // Build the client object
+$hosts = ['127.0.0.1:9200'];            // Replace with your host
+$client = ClientBuilder::create()       // Instantiate a new ClientBuilder
+	->setHosts($hosts)      	// Set the hosts
+    	->build();              	// Build the client object
 
-// $response = $client->search([...]);  // Execute a search with the new client
-
-
-// $es = new Elasticsearch\Client([
-// 	'hosts' => ['127.0.0.1:9200']
-// ]);
 
 ?>
